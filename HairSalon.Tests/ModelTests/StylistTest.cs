b3d292c51bc4cd6543508f6ed8beaa09f4067288
Lan Dam <ldam77@yahoo.com>
2018-07-13 09:34:51 -0700

    Add stylist class with getters and test

---
+++ HairSalon.Tests/ModelTests/StylistTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class StylistTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+    public StylistTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=lan_dam_test;";
+    }
+    [TestMethod]
+    public void GetTest_ReturnDataField()
+    {
+      // Arrange
+      int id = 1;
+      string name = "testName";
+      Stylist testStylist = new Stylist(name, id);
+
+      // Act
+      int resultId = testStylist.GetId();
+      string resultName = testStylist.GetName();
+
+      // Assert
+      Assert.AreEqual(id, resultId);
+      Assert.AreEqual(name, resultName);
+    }
+  }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using HairSalon;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    private int id;
+    private string name;
+
+    public Stylist(string newName, int newId = 0)
+    {
+      name = newName;
+      id = newId;
+    }
+
+    public int GetId()
+    {
+      return id;
+    }
+
+    public string GetName()
+    {
+      return name;
+    }
+
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM stylists;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+  }
+}
