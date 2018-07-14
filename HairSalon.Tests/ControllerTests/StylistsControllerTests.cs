6414cecf578ce928f8bad4f5587573728fa0650f
Lan Dam <ldam77@yahoo.com>
2018-07-13 21:41:13 -0700

    Add remaining controller tests

--- HairSalon.Tests/ControllerTests/StylistsControllerTests.cs
+++ HairSalon.Tests/ControllerTests/StylistsControllerTests.cs
@@ -33,5 +33,41 @@ namespace HairSalon.Tests
       //Assert
       Assert.IsInstanceOfType(result, typeof(List<Stylist>));
     }
+    [TestMethod]
+    public void NewStylist_ReturnsCorrectView_True()
+    {
+      //Arrange
+      StylistsController controller = new StylistsController();
+
+      //Act
+      ActionResult indexView = controller.NewStylist();
+
+      //Assert
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void StylistDetail_ReturnsCorrectView_True()
+    {
+      //Arrange
+      StylistsController controller = new StylistsController();
+
+      //Act
+      ActionResult indexView = controller.StylistDetail("testName");
+
+      //Assert
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void StylistDetail_HasCorrectModelType_StylistList()
+    {
+      //Arrange
+      ViewResult indexView = new StylistsController().StylistDetail("testName") as ViewResult;
+
+      //Act
+      var result = indexView.ViewData.Model;
+
+      //Assert
+      Assert.IsInstanceOfType(result, typeof(Stylist));
+    }
   }
 }
